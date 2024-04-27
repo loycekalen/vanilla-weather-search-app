d09faa569886bd481f606744124e96c3aaf8483d
loycekalen <166914558+loycekalen@users.noreply.github.com>
2024-04-27 22:57:09 +0300

    added search engine and axios

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function weatherDisplay(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let cityElement = document.querySelector("h1");
+  cityElement.innerHTML = searchInput.value;
+}
 
+let searchForm = document.querySelector("#weather-search");
+searchForm.addEventListener("submit", weatherDisplay);
+
+function showWeather(response) {}
+let apiKey = "1485fbda2c63be71004148t8363fo230";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(showWeather);
