adab2f6969e3408ffd8f5d718742898191008005
loycekalen <166914558+loycekalen@users.noreply.github.com>
2024-04-27 23:30:10 +0300

    added search functionality to the API

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
+function updateWeatherInfo(response) {
+  let temperatureElement = document.querySelector("#weather-value");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("h1");
+
+  cityElement.innerHTML = response.data.city;
+}
+
+function citySearch(city) {
+  let apiKey = "1485fbda2c63be71004148t8363fo230";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeatherInfo);
+}
+
 function weatherDisplay(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("h1");
-  cityElement.innerHTML = searchInput.value;
+
+  citySearch(searchInput.value);
 }
 
 let searchForm = document.querySelector("#weather-search");
 searchForm.addEventListener("submit", weatherDisplay);
 
-function showWeather(response) {}
-let apiKey = "1485fbda2c63be71004148t8363fo230";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-
-axios.get(apiUrl).then(showWeather);
+citySearch("London");
